1f352ad45f7acd7089cc1393e3655354bab9be6b
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2020-10-26 18:21:27 +0100

    zavrsena main metoda

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,9 +1,23 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        int n;
+        Scanner ulaz = new Scanner(System.in);
 
+        System.out.println("Unesite broj n: ");
+        n = ulaz.nextInt();
+
+        System.out.println("Brojevi u opsegu [1," + n + "] koji su djeljivi sa sumom cifara " + n + " su:");
+
+        int nSumaCifara = sumaCifara(n);
+
+        for (int i = 1; i <= n; ++i) {
+            if (i % nSumaCifara == 0) System.out.println(i);
+        }
     }
 
     public static int sumaCifara(int n) {
